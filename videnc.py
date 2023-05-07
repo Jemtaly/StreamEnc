10225692abb1bed6d27efff2614e3aef976e1438
Jemtaly <83796250+Jemtaly@users.noreply.github.com>
2023-05-07 12:13:21 +0800

    Make Decrypter window always on top

--- videnc.py
+++ videnc.py
@@ -45,6 +45,7 @@ def screen(key, size, len):
         iSrc = ImageGrab.grab()
         iDst = decrypt(key, iSrc.resize(size), len)
         cv2.imshow('Decrypter', cv2.cvtColor(np.array(iDst), cv2.COLOR_RGB2BGR))
+        cv2.setWindowProperty('Decrypter', cv2.WND_PROP_TOPMOST, 1)
         cv2.waitKey(1)
         if cv2.getWindowProperty('Decrypter', cv2.WND_PROP_VISIBLE) < 1:
             break
