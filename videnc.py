4d44ed42f5d57ef504b64f573d2903cad02201d7
Jemtaly <83796250+Jemtaly@users.noreply.github.com>
2023-06-24 17:44:35 +0800

    Add files via upload

--- videnc.py
+++ videnc.py
@@ -13,10 +13,11 @@ def encrypt(key, iSrc, len):
     iDst = Image.frombytes('RGB', iSrc.size, bDst)
     for i in range(8):
         for j in range(8):
-            bit = (nonce[i] >> j & 1) * 255
+            bit = nonce[i] >> j & 1
+            rgb = 255 * bit, 255 * bit, 255 * bit
             for x in range(len):
                 for y in range(len):
-                    iDst.putpixel((i * len + x, j * len + y), bit)
+                    iDst.putpixel((i * len + x, j * len + y), rgb)
     return iDst
 def decrypt(key, iSrc, len):
     iSrc = iSrc.convert('RGB')
@@ -24,7 +25,9 @@ def decrypt(key, iSrc, len):
     nonce = bytearray(8)
     for i in range(8):
         for j in range(8):
-            nonce[i] |= iSrc.getpixel((i * len + len // 2, j * len + len // 2))[0] >> 7 << j
+            rgb = iSrc.getpixel((i * len + len // 2, j * len + len // 2))
+            bit = sum(rgb) // 383
+            nonce[i] |= bit << j
     cipher = AES.new(key, AES.MODE_CTR, nonce = nonce)
     bDst = cipher.decrypt(bSrc)
     iDst = Image.frombytes('RGB', iSrc.size, bDst)
